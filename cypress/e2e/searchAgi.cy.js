abf17435f570c52ea7ac5ee132bf852cd51476e9
Erik Noriki <eriknoriki@hotmail.com>
2023-01-24 10:56:20 -0300

    Realizando melhorias no código

--- cypress/e2e/searchAgi.cy.js
+++ cypress/e2e/searchAgi.cy.js
@@ -1,21 +1,21 @@
 import searchAgi from "../pages/searchAgi"
 
 describe('Fluxo de pesquisa de artigo no blog Agi', function() {
-    it('CE01: Realizar pesquisa de artigo no blog Agi com a palavra-chave: mercado', function(){
-        searchAgi.acessaSite();
-        searchAgi.realizaPesquisa();
-        searchAgi.resultadoPesquisa();
+    it('CE01: Realiza pesquisa de artigo no blog Agi com a palavra-chave: mercado', function(){
+        searchAgi.accessAgi();
+        searchAgi.doSearch();
+        searchAgi.searchResult();
     })
 
-    it('CE02: Realizar pesquisa de artigo no blog Agi com a palavra-chave inválida ou não existente', function(){
-        searchAgi.acessaSite();
-        searchAgi.realizaPesquisaInvalida();
-        searchAgi.resultadoPesquisaInvalida();
+    it('CE02: Realiza pesquisa de artigo no blog Agi com a palavra-chave inválida ou não existente', function(){
+        searchAgi.accessAgi();
+        searchAgi.doInvalidSearch();
+        searchAgi.searchInvalidResult();
     })
 
-    it('CE03: Realizar pesquisa de artigo no blog Agi com a palavra-chave vazia', function(){
-        searchAgi.acessaSite();
-        searchAgi.realizaPesquisaVazia();
-        searchAgi.resultadoPesquisaVazia();
+    it('CE03: Realiza pesquisa de artigo no blog Agi com a palavra-chave vazia', function(){
+        searchAgi.accessAgi();
+        searchAgi.doEmptyFieldSearch();
+        searchAgi.searchEmptyFieldResult();
     })
 })
--- cypress/pages/searchAgi.js
+++ cypress/pages/searchAgi.js
@@ -6,38 +6,41 @@ var year = date.getFullYear();
 var monthYearNow = month + '/' + year;
 
 class realizaPesquisa {
-    acessaSite(){
+    accessAgi(){
         cy.visit(url);
     }
 
-    realizaPesquisa(){
+    doSearch(){
         cy.get(searchElements.SEARCH_BUTTON).first().click();
         cy.get(searchElements.SEARCH_FIELD).first().focus().type('mercado');
         cy.get(searchElements.SEARCH_SUBMIT).first().click();
     }
 
-    realizaPesquisaInvalida(){
+    doInvalidSearch(){
         cy.get(searchElements.SEARCH_BUTTON).first().click();
         cy.get(searchElements.SEARCH_FIELD).first().focus().type('%%%%');
         cy.get(searchElements.SEARCH_SUBMIT).first().click();
     }
 
-    realizaPesquisaVazia(){
+    doEmptyFieldSearch(){
         cy.get(searchElements.SEARCH_BUTTON).first().click();
         cy.get(searchElements.SEARCH_SUBMIT).first().click();
     }
 
-    resultadoPesquisa(){
+    searchResult(){
+        cy.url().should('include', '?s=mercado')
         cy.get(searchElements.RESULT_TITLE).should('have.text', 'Resultados da busca por: mercado')
         cy.get(searchElements.SEARCH_RESULTS).should('contain', 'mercado');
     }
 
-    resultadoPesquisaInvalida(){
+    searchInvalidResult(){
+        cy.url().should('include', '?s=%25%25%25%25')
         cy.get(searchElements.INVALID_RESULT_TITLE).should('have.text', 'Nenhum resultado')
         cy.get(searchElements.SEARCH_INVALID_RESULTS).should('contain', 'Não encontramos nada para estes termos de busca. Tente novamente com palavras-chave diferentes.');
     }
 
-    resultadoPesquisaVazia(){
+    searchEmptyFieldResult(){
+        cy.url().should('include', '?s=')
         cy.get(searchElements.RESULT_TITLE).should('have.text', 'Resultados da busca por: ')
         cy.get(searchElements.SEARCH_RESULTS).should('contain', monthYearNow);
     }
